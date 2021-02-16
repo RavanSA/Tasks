f3a0803cc6b62d304adad9e3fa7b6bbbf01c30de
RavanSA <58227337+RavanSA@users.noreply.github.com>
2021-02-16 23:01:32 +0400

    Update task1.php

--- 16.02.2021/task1.php
+++ 16.02.2021/task1.php
@@ -1,14 +1,12 @@
-﻿<?php 
+<?php 
 
     function arr_cedvel( $arr ) {
-
        echo "<table border = 1>
             <tr>
               <th>Ad, Soyad</th>
               <th>Yas</th>
               <th>Cins</th>
             </tr>";
-
        for ( $row = 0; $row < sizeof($arr); $row++ ) {
          echo "<tr>";
          for ( $col = 0; $col < 3; $col++ ) {
@@ -18,6 +16,7 @@
        }
        echo "</table>";
     }
+
     
     function yas_ortalamasi( $arr ){
       $yasortalamasi = 0;
@@ -27,8 +26,9 @@
       }
 
       $yasortalamasi /= sizeof( $arr );
-      return $yasortalamasi;
+      return ceil($yasortalamasi);
     }
+
 
     function yas_boyuk($arr){
       $yasboyuk=$arr[1][1];
@@ -42,6 +42,7 @@
       return $adboyuk;
     }
 
+
     function yas_kicik($arr){
       $yaskicik=$arr[1][1];
       $adkicik =" ";
@@ -54,35 +55,50 @@
       return $adkicik;
     }
 
-    function oglan_qiz_sayi($arr){
-      $cinsisay=0;
+
+    function oglan_sayi($arr){
+      $sayK=0;
       for ($row = 0; $row < sizeof($arr); $row++) {
-          if($arr[$row][2]== "q" || $arr[$row][2]== "k" ){
-              $cinsisay+=1;
+          if($arr[$row][2]== "k" ){
+              $sayK+=1;
           }
       }
-      return $cinsisay;
+      return $sayK;
     }
+
+
+    function qiz_sayi($arr){
+      $sayQ=0;
+      for ($row = 0; $row < sizeof($arr); $row++) {
+          if($arr[$row][2]== "q" ){
+              $sayQ+=1;
+          }
+      }
+      return $sayQ;
+    }
+
 
     function sinif_sayi($arr){
       return sizeof($arr);
     }
 
+
     $array = array (
              array("Nermin Eliyeva",10,"q"),
-             array("Kamil Rahimli",22,"basqa"),
+             array("Kamil Rahimli",22,"k"),
              array("Zaur Qurbaneliyev",12,"k"),
              array("Melahet Isgenderli",39,"q"),
              array("Ali Rzayev",13,"k"),
              array("Ramin Orucov",52,"k"),
              array("Veli Kerimli",14,"k"),
-             array("Zamiq Aliyev" ,3,"q"),
-             array("Esmaye Mustafayeva" ,12,"basqa"),
+             array("Zamiq Aliyev" ,3,"k"),
+             array("Esmaye Mustafayeva" ,12,"q"),
              array("Veli KERIMLI" ,51,"k")
              );
 
       arr_cedvel($array);
-      echo "Sinifdeki oglanlarin ve gizlarin sayi: ".oglan_qiz_sayi($array)."<br>";
+      echo "Sinifdeki oglanlarin sayi: ".oglan_sayi($array)."<br>";
+      echo "Sinifdeki qizlarin sayi: ".qiz_sayi($array)."<br>";
       echo "Sinifdeki usaqlarin umumi yas ortalamasi: ".yas_ortalamasi($array)."<br>";
       echo "Sinifdeki en boyuk usaq: ".yas_boyuk($array)."<br>"."Sinifdeki en kicik usaq: ".yas_kicik($array);
       echo "<br>"."Sinifdeki usaqlarin umumi sayi: ".sinif_sayi($array);
